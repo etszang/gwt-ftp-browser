39bf2d8d01725b79f1a37df894ade11cb4f062c5
jonkelling <jonkelling@6326471e-f82b-0410-9dcc-0da7c566b97b>
2007-04-01 19:37:28 +0000

    Adding these so that Tree and TreeItem can be used one-in-the-same for addItem(TreeItem) and removeItems() yay!!

---
+++ trunk/src/com/jonosoft/ftpbrowser/web/client/HasTreeItems.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.jonosoft.ftpbrowser.web.client;
+
+/**
+ * @author Jkelling
+ *
+ */
+public interface HasTreeItems {
+	
+	public void addItem(com.google.gwt.user.client.ui.TreeItem item);
+	public void removeItems();
+	
+}
---
+++ trunk/src/com/jonosoft/ftpbrowser/web/client/Tree.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.jonosoft.ftpbrowser.web.client;
+
+
+/**
+ * @author Jkelling
+ *
+ */
+public class Tree extends com.google.gwt.user.client.ui.Tree implements HasTreeItems {
+	
+	public Tree() {
+		super();
+	}
+	
+}
